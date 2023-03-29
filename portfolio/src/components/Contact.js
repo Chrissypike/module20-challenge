e1867ee508606952a3db00d09161c4a19b1ecaea
Chrissypike <you@example.com>
2023-03-29 18:34:50 -0500

    contact.js file and code

---
+++ portfolio/src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const styles = {
+    navbarStyle: {
+        backgroundColor: '#AE759F',
+        padding: '20px',
+        textAlign: 'center',
+    },
+    Contact: {
+        fontFamily: 'Fontdinerdotcom Sparkly',
+    }
+}
+
+function Contact() {
+    return (
+        <div id='contact' style={styles.navbarStyle}>
+            <h1 style={styles.Contact}>Contact</h1>
+            <form>
+                <label>Name:</label>
+                <input
+                    label='Name'
+                    type='text'
+                />
+            </form>
+
+        </div>
+    );
+}
+
+export default Contact;
