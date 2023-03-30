6a5eb2032a3ffe2891ee84faa5ec6600dba2d88d
Chrissypike <you@example.com>
2023-03-29 19:56:21 -0500

    footer.js added in app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Portfolio />
       <Contact />
       <Resume />
+      <Footer />
     </div>
   );
 }
